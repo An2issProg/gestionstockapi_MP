df7cfddb9b1ffa279a019cad5a3c16ae5e05d549
An2issProg <a.mybuisnesscollaboration@gmail.com>
2025-05-19 23:14:24 +0200

    API ANIS

---
+++ src/main/java/com/bouali/gestiondestock/validator/LigneCommandeClientValidator.java
@@ -0,0 +1,15 @@
+package com.bouali.gestiondestock.validator;
+
+import com.bouali.gestiondestock.dto.LigneCommandeClientDto;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LigneCommandeClientValidator {
+
+
+  public static List<String> validate(LigneCommandeClientDto dto) {
+    List<String> errors = new ArrayList<>();
+    return errors;
+  }
+
+}
